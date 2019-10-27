c7690f1132c8f9e14dca1cdb0ce8adb3b0ab090d
Rodrigo Fabricante <rodrigo.fabricante@b2wdigital.com>
2019-10-27 12:18:42 -0300

    init so

--- main.py
+++ main.py
@@ -20,16 +20,30 @@ class Process:
 
 class SO:
     def __init__(self):
-        self.hd = HD()
-        self.ram = RAM()
+        self.hd = HD(1)
+        self.ram = RAM(1)
         self.processes = []
     
-    def execute(self, process_id, command, end):
-        pass
+    def execute(self, process_id, command, end=None):
+        if command == 'P':
+            pass
+        elif command == 'I':
+            pass
+        elif command == 'C':
+            pass
+        elif command == 'R':
+            pass
+        elif command == 'W':
+            pass 
+        elif command == 'T':
+            pass
+        else:
+            print("Invalid command '{}'. Exiting...".format(command))
+            raise SystemExit()
 
     def read_instructions(self):
         instructions = []
-        for i in range(20):
+        for i in range(1):
             instructions.append(input())
         return instructions
 
@@ -37,5 +51,5 @@ if __name__ == '__main__':
     so = SO()
     instructions = so.read_instructions()
     for i in instructions:
-        process_id, command, end = i.split()
+        process_id, command, *end = i.split()
         so.execute(process_id, command, end)
